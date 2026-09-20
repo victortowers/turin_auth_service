9391b527f8145512c2a3e9025d9d0c1d227eeb67
vchen <202715235+victortowers@users.noreply.github.com>
2026-09-20 19:06:23 -0300

    b2.py is the new file, will try to deploy my app with Vercel.

---
+++ turin_graph/b1.py
@@ -0,0 +1,124 @@
+from platform import node
+from time import monotonic
+
+import numpy as np
+import scipy.sparse as sp
+import math
+import heapq
+import time
+
+def find_position(node_idx):
+    position = int(np.searchsorted(node_ids, node_idx))
+    assert node_ids[position] == node_idx
+    return position
+
+def reverse_position(position_x):
+    return int(node_ids[position_x])
+
+def find_node(indice_x, data_x):
+      try:
+            next_nodes = []
+            for x, y in zip(indice_x, data_x):
+                  new_x = node_ids[x]
+                  next_nodes.append([new_x, y])
+
+            return next_nodes, len(next_nodes)
+      except Exception:
+            return None
+
+def return_node(array_location):
+      data2 = data[indptr[array_location]:indptr[array_location + 1]]
+      indices2 = indices[indptr[array_location]:indptr[array_location + 1]]
+
+      return indices2, data2
+
+def node_coordinates(node_psx):
+    return coordinates[node_psx]
+
+def radians(dict1):
+    return np.radians(dict1)
+
+def heuristic(node_idx, lat2, lon2):
+    candidate_coords = node_coordinates(node_idx)
+    lat1, lon1 = radians(candidate_coords)
+    distance_lat = lat2 - lat1
+    distance_lon = lon2 - lon1
+
+    a = math.sin(distance_lat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(distance_lon/2)**2
+    return 2 * 6371 * math.asin(math.sqrt(a))
+
+def a_star(start1, end1):
+    start_x = find_position(start1)
+    end_x = find_position(end1)
+    end_lat, end_lon = radians(node_coordinates(end_x))
+    d1 = heuristic(start_x, end_lat, end_lon)
+
+    print(f"Route: OSM {start1} ({start_x}) to OSM {end1} ({end_x})")
+    print(f"Straight distance is {round(d1, 4)} kilometres")
+
+    priority_queue = [(d1, 0.0, start_x, [start_x])]
+    pq = priority_queue
+
+    visited = set()
+    nodes_explored = 0
+
+    while pq:
+        estimated, distance_travelled, current_row, row_path = heapq.heappop(pq)
+        nodes_explored +=1
+        if nodes_explored % 10000 == 0:
+            print(f"Distance Remaining is {estimated - distance_travelled:.2f}km, travelled {distance_travelled:.2f}km")
+
+        if current_row == end_x:
+            osm_path = [reverse_position(r) for r in row_path]
+            print(f"✅ Found! {nodes_explored:,} nodes explored, {len(osm_path):,} hops")
+            return distance_travelled, osm_path
+
+        if current_row in visited:
+            continue
+        visited.add(current_row)
+
+        next_candidates, next_weights = return_node(current_row)
+        #print(f" Trying Candidates {next_candidates}")
+        #print(f"Trying next distances{next_weights}")
+
+        for i in range(len(next_candidates)):
+            node_x = int(next_candidates[i])
+            if node_x in visited: continue
+            weight_x = float(next_weights[i])
+
+            new_distance = distance_travelled + weight_x
+            h1 = heuristic(node_x, end_lat, end_lon)
+
+            heapq.heappush(pq, (new_distance + h1, new_distance, node_x, row_path + [node_x]))
+
+coordinates = np.load(".osm_cache/node_coords.npy",  mmap_mode="r") #1
+node_ids = np.load(".osm_cache/node_ids.npy",  mmap_mode="r")
+indices = np.load(".osm_cache/indices.npy",  mmap_mode="r") #2
+
+data = np.load(".osm_cache/data.npy",  mmap_mode="r") #3
+shape = np.load(".osm_cache/shape.npy", mmap_mode="r")
+indptr = np.load(".osm_cache/indptr.npy",  mmap_mode="r") #4
+
+
+A = sp.csr_matrix((data, indices, indptr), shape=tuple(shape))
+
+# --- basic stats ---
+print("nodes:", A.shape[0], "| directed edges:", A.nnz,"| avg degree: %.2f" % (A.nnz / A.shape[0]))
+
+#start = 1871769061
+#start = 31935580 # (Aeroporto de Viracopos)
+#start = 1001568698 # (Residência Cidade Jardim)
+#start = 2368042870 # (Santa Bárbara Residence)
+#start = 1871768924 # (Alameda Coinbra)
+#end = 4363722848 # (Saída Alpha 0)
+#start = 245374595 # (Aeroporto de Guarulhos)
+start = 4137343158 # (Fazenda Boa Vista)
+#start = 2868730635 # (Vila Galé Angra dos Reis)
+#end = 493141051 # (Praia Grande)
+#end = 1669971805 # (Taubaté)
+end = 12099764350 # (Shopping Village Mall, Rio de Janeiro)
+time1 = time.time_ns()
+total_distance, routed = a_star(start, end)
+print(total_distance)
+total_time = (time.time_ns() - time1) / (10 ** 9)
+print(f"Time taken is {total_time:.3f} seconds")
